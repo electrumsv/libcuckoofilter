db514e237007541ba38f222feaf4f8d2ed868e79
Roger Taylor <roger.taylor.email@gmail.com>
2022-02-18 11:10:48 +1300

    Update the setup with non-temporary values that will be shown in the PyPI page.

--- setup.py
+++ setup.py
@@ -2,20 +2,18 @@ from distutils.core import setup, Extension
 import sys
 
 module1 = Extension('bsvcuckoo',
-                    define_macros=[('MAJOR_VERSION', '1'),
-                                   ('MINOR_VERSION', '0')],
                     include_dirs=['include'],
                     sources=["src/cuckoo_filter.c", "src/cuckoo_python.c"],
                     # https://cibuildwheel.readthedocs.io/en/stable/faq/#windows-importerror-dll-load-failed-the-specific-module-could-not-be-found
                     extra_compile_args=['/d2FH4-'] if sys.platform == 'win32' else [])
 
 setup(name='bsvcuckoo',
-      version='1.0',
+      version='1.01',
       description='A cuckoo filter implementation.',
       author='Roger Taylor',
       author_email='roger.taylor.email@gmail.com',
-      url='https://example.com',
-      long_description='''
-TBD
-''',
+      url='https://github.com/electrumsv/libcuckoofilter',
+      long_description=open('README.md', 'r').read(),
+      long_description_content_type='text/markdown',
+      license='MIT Licence',
       ext_modules=[module1])
